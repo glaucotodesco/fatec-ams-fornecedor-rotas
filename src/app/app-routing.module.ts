8c3eeb3162ebaeb2faca12b9044dd635b8aab355
glaucotodesco <glaucotodesco@gmail.com>
2023-05-16 15:09:54 -0300

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { SupplierComponent } from './supplier/supplier.component';
+import { ClientComponent } from './client/client.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+   {path : 'home', component: HomeComponent},
+   {path :  'supplier', component: SupplierComponent },
+   {path :  'client', component: ClientComponent },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
